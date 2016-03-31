48022a8e270a3e71177ec645fcc042aed770b8c9
Lithrem <lithrem@kororaproject.org>
2016-03-31 13:56:57 +1100

    updated: Add initial Dockerfiles for database and Web

---
+++ docker/db/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (username, email, password, status) VALUES ('test1', 'test@kororaproject.org','$P$BlfJsb9tY5r1prQQy8NAcqn2i.vc/s/', 'active');
+INSERT INTO users (username, email, password, status) VALUES ('test2', 'test@kororaproject.org','$P$BlfJsb9tY5r1prQQy8NAcqn2i.vc/s/', 'active');
+INSERT INTO users (username, email, password, status) VALUES ('test3', 'test@kororaproject.org','$P$BlfJsb9tY5r1prQQy8NAcqn2i.vc/s/', 'active');
